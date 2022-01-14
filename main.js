f21e27b2e43625a87cbd090032c040254ce17666
Dustin-Davis <drpookydavis@comcast.net>
2022-01-14 12:04:41 -0800

    fixed some issues

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var addbutton = document.querySelector('.addbutton');
-var overlay = document.querySelector('.overlay h');
-var modal = document.querySelector;
+var overlay = document.querySelector('.overlay.h');
+var modal = document.querySelector('.modal.h');
 addbutton.addEventListener('click', function () {
-
+  modal.className = 'modal';
+  overlay.className = 'overlay';
 });
