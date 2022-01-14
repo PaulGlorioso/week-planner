5c1d84198a6553f5cc755006d66e4fa5dadfb07a
Jeesu Park <jeesupark96@gmail.com>
2022-01-14 11:46:02 -0800

    modal

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
 var addbutton = document.querySelector('.addbutton');
+var overlay = document.querySelector('.overlay h');
+var modal = document.querySelector;
+addbutton.addEventListener('click', function () {
+
+});
